6385a40858afcd0f9bd87890d2b5ee7efb349aa6
Swannhs <princeapurbask@gmail.com>
2021-12-22 21:51:31 +0600

    Added password encoder as BCryptPasswordEncoder

--- src/main/java/com/example/industrialattachment/security/SecurityConfig.java
+++ src/main/java/com/example/industrialattachment/security/SecurityConfig.java
@@ -1,11 +1,13 @@
 package com.example.industrialattachment.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -27,5 +29,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .permitAll()
                 .anyRequest()
                 .authenticated();
+    }
+
+    @Bean
+    BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }

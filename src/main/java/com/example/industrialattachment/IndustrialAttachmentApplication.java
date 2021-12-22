ea588282a7112e45b7c1d8627936fa3d3489e4b1
Swannhs <princeapurbask@gmail.com>
2021-12-22 21:13:32 +0600

    initialize commit

---
+++ src/main/java/com/example/industrialattachment/IndustrialAttachmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.industrialattachment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndustrialAttachmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IndustrialAttachmentApplication.class, args);
+    }
+
+}

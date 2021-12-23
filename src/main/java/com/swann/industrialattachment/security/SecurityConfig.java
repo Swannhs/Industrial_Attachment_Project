55ed457807b403099062be249301bdc72ea84d2f
Swannhs <princeapurbask@gmail.com>
2021-12-23 14:37:51 +0600

    Configured bean for auth handling

--- src/main/java/com/swann/industrialattachment/security/SecurityConfig.java
+++ src/main/java/com/swann/industrialattachment/security/SecurityConfig.java
@@ -1,7 +1,12 @@
 package com.swann.industrialattachment.security;
 
+import com.swann.industrialattachment.service.UserDetailsServiceImp;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -17,10 +22,29 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
         prePostEnabled = true
 )
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private JwtAuthenticationEntryPoint unAuthHandler;
+
+    @Autowired
+    private UserDetailsServiceImp userDetailsServiceImp;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsServiceImp);
+    }
+
+    @Override
+    @Bean(BeanIds.AUTHENTICATION_MANAGER)
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .cors().disable().csrf().disable()
+                .exceptionHandling().authenticationEntryPoint(unAuthHandler)
+                .and()
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
